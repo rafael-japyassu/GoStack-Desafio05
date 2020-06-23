4fb8733a040f909b6517b6e70d4b6a55cb291bc8
Rafael Vasconcelos <rafaelvasconcelos@MacBook-Pro-de-Rafael.local>
2020-06-23 12:06:05 -0300

    Finished challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,47 @@ interface Balance {
   total: number;
 }
 
+interface ListTransaction {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = { income: 0, outcome: 0, total: 0 };
   }
 
-  public all(): Transaction[] {
+  public all(): ListTransaction {
     // TODO
+    const transactions = {
+      transactions: this.transactions,
+      balance: this.balance,
+    };
+
+    return transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    this.balance[type] += value;
+    this.balance.total = this.balance.income - this.balance.outcome;
+    return transaction;
   }
 }
 
